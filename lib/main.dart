fed3a68fd19ea2a8af8c447fca4e787e4579b80d
Ammar Talal <103538665+FatalError98@users.noreply.github.com>
2024-05-06 18:50:26 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'counter_observer.dart';
 import 'app.dart';
 
 void main() {
-  Bloc.observer = const CounterObserver();
   runApp(CounterApp());
 }
